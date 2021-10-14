e4da0b9dac778e9c7b0369f5d815bb02da120c86
rahmiddinasiydinov <rahmiddinasiydinov>
2021-10-14 06:36:06 +0500

    initial choice

--- utils.js
+++
@@ -1,8 +0,0 @@
-let createEl = (para) =>document.createElement(para);
-let $ = (para, child=document) => child.querySelector(para);
-
-let dateConvert = (x)=>{
-    let time = new Date(x);
-     
-    return  time.getDate() + '.' + time.getMonth() + 1 + '.' + time.getFullYear(); 
-}
